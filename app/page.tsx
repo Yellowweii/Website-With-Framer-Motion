ac46c999f4afeb59c8da8eeaaca8ee12dd466e84
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-10-28 21:22:52 +0800

    finish the About module (#2)
    * finish the nav module
    
    * finish the About module

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import About from "@/components/About";
+import Intro from "@/components/Intro";
+import SectionDivider from "@/sections/Section-divider";
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+      <About />
     </main>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    domains: ["www.yellowwei.cn"],
+  },
+};
 
 module.exports = nextConfig;
