0813c9c2d2d82c0fc2b10f52889ce2ca520d2588
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-10-30 09:14:54 +0800

    use the context api (#4)
    * finish the nav module
    
    * finish the About module
    
    * finish the project module
    
    * use the context api

--- sections/Header/index.tsx
+++ sections/Header/index.tsx
@@ -3,8 +3,11 @@ import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { links } from "@/lib/data";
+import clsx from "clsx";
+import { useActiveSectionContext } from "@/context/ActiveSectionContextProvider";
 
 const Header = () => {
+  const { activeSection, setActiveSection } = useActiveSectionContext();
   return (
     <header className="z-[999] relative">
       <motion.div
@@ -16,8 +19,23 @@ const Header = () => {
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li className="h-3/4 flex items-center justify-center relative" key={link.hash} initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
-              <Link className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition" href={link.hash}>
+              <Link
+                className={clsx("flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition", { "text-gray-950": activeSection === link.name })}
+                href={link.hash}
+                onClick={() => setActiveSection(link.name)}
+              >
                 {link.name}
+                {activeSection === link.name && (
+                  <motion.span
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
+                )}
               </Link>
             </motion.li>
           ))}
