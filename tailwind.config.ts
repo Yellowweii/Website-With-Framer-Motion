500518dbcf7c93c37dfcdca796c6bffd9202b324
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-11-04 11:59:25 +0800

    little change (#10)
    * finish the nav module
    
    * finish the About module
    
    * finish the project module
    
    * use the context api
    
    * custom my hook
    
    * finish the skills module
    
    * finish the Experience module
    
    * finish some function of Email_send
    
    * finish the 80 percent of the project
    
    * little change , do nothing basically

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./sections/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
@@ -11,5 +10,7 @@ const config: Config = {
     },
   },
   plugins: [],
+  darkMode: "class",
 };
-export default config;
+
+export {};
