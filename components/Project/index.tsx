e6cba8b88f340ae4af433522ff9e5290f016e735
Wei  Huang <138266079+Yellowweii@users.noreply.github.com>
2023-11-18 10:59:17 +0800

    fix a bug (#17)
    * finish the nav module
    
    * finish the About module
    
    * finish the project module
    
    * use the context api
    
    * custom my hook
    
    * finish the skills module
    
    * finish the Experience module
    
    * finish some function of Email_send
    
    * finish the 80 percent of the project
    
    * little change , do nothing basically
    
    * finish some sections' dark mode
    
    * finish the whole project, successfully accomplished
    
    * add a seo sign
    
    * change the favicon.ico
    
    * change the title
    
    * change the about me module and projects module
    
    * fix a bug

--- components/Project/index.tsx
+++ components/Project/index.tsx
@@ -16,6 +16,7 @@ const Product = ({ title, description, tags, imageUrl }: ProductProps) => {
     target: ref,
     offset: ["0 1", "1.33 1"],
   });
+
   const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
   return (
