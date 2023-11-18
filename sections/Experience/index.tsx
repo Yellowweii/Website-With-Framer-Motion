6ff08298a83697309f8fc1827e1a13e115ad1338
huang wei <1119765689hw@gmail.com>
2023-11-18 10:56:44 +0800

    fix a bug

--- sections/Experience/index.tsx
+++ sections/Experience/index.tsx
@@ -1,47 +1,59 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import SectionHeading from "@/components/SectionHeading";
 import { useTheme } from "@/context/ThemeContext";
+import { useInView } from "framer-motion";
+import { useActiveSectionContext } from "@/context/ActiveSectionContextProvider";
 
 export default function Experience() {
-  const { ref, inView } = useSectionInView("Experience", 0.6, true);
+  const ref_two = React.useRef(null);
+  const inView_two = useInView(ref_two, { amount: 0.6, once: true });
+  const { setActiveSection } = useActiveSectionContext();
+  const { ref } = useSectionInView("Experience", 0.6);
   const { theme } = useTheme();
+  useEffect(() => {
+    if (inView_two) {
+      setActiveSection("Experience");
+    }
+  }, [inView_two]);
   return (
-    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
-      <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline lineColor="">
-        {experiencesData.map((item, index) => (
-          <React.Fragment key={index}>
-            <VerticalTimelineElement
-              visible={inView}
-              contentStyle={{
-                background: theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
-                boxShadow: "none",
-                border: "1px solid rgba(0, 0, 0, 0.05)",
-                textAlign: "left",
-                padding: "1.3rem 2rem",
-              }}
-              contentArrowStyle={{
-                borderRight: theme === "light" ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255, 255, 255, 0.5)",
-              }}
-              date={item.date}
-              icon={item.icon}
-              iconStyle={{
-                background: theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
-                fontSize: "1.5rem",
-              }}
-            >
-              <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="font-normal !mt-0">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">{item.description}</p>
-            </VerticalTimelineElement>
-          </React.Fragment>
-        ))}
-      </VerticalTimeline>
-    </section>
+    <div ref={ref_two}>
+      <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
+        <SectionHeading>My experience</SectionHeading>
+        <VerticalTimeline lineColor="black">
+          {experiencesData.map((item, index) => (
+            <React.Fragment key={index}>
+              <VerticalTimelineElement
+                visible={inView_two}
+                contentStyle={{
+                  background: theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+                  boxShadow: "none",
+                  border: "1px solid rgba(0, 0, 0, 0.05)",
+                  textAlign: "left",
+                  padding: "1.3rem 2rem",
+                }}
+                contentArrowStyle={{
+                  borderRight: theme === "light" ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255, 255, 255, 0.5)",
+                }}
+                date={item.date}
+                icon={item.icon}
+                iconStyle={{
+                  background: theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <h3 className="font-semibold capitalize">{item.title}</h3>
+                <p className="font-normal !mt-0">{item.location}</p>
+                <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">{item.description}</p>
+              </VerticalTimelineElement>
+            </React.Fragment>
+          ))}
+        </VerticalTimeline>
+      </section>
+    </div>
   );
 }
