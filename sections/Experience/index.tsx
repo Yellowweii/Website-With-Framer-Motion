c29577b002fb3251cba18570ae21a6c7860a0d79
Wei  Huang <138266079+Yellowweii@users.noreply.github.com>
2023-11-19 15:15:48 +0800

    update the experience module (#19)
    * finish the nav module
    
    * finish the About module
    
    * finish the project module
    
    * use the context api
    
    * custom my hook
    
    * finish the skills module
    
    * finish the Experience module
    
    * finish some function of Email_send
    
    * finish the 80 percent of the project
    
    * little change , do nothing basically
    
    * finish some sections' dark mode
    
    * finish the whole project, successfully accomplished
    
    * add a seo sign
    
    * change the favicon.ico
    
    * change the title
    
    * change the about me module and projects module
    
    * fix a bug
    
    * finish the technology exhibition module
    
    * update the experience module

--- sections/Experience/index.tsx
+++ sections/Experience/index.tsx
@@ -24,7 +24,7 @@ export default function Experience() {
     <div ref={ref_two}>
       <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
         <SectionHeading>My experience</SectionHeading>
-        <VerticalTimeline lineColor={theme === "light" ? "black" : ""}>
+        <VerticalTimeline lineColor="">
           {experiencesData.map((item, index) => (
             <React.Fragment key={index}>
               <VerticalTimelineElement
@@ -46,7 +46,7 @@ export default function Experience() {
                   fontSize: "1.5rem",
                 }}
               >
-                <h3 className="font-semibold capitalize">{item.title}</h3>
+                <h3 className="text-lg font-semibold capitalize">{item.title}</h3>
                 <p className="font-normal !mt-0">{item.location}</p>
                 <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">{item.description}</p>
               </VerticalTimelineElement>
